68374ff4e86d9b0bad39dc8d00d521f5ff67e793
Guillermo Gette <guillermo@workast.io>
2022-11-09 22:01:13 +1100

    Break long error messages

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,7 +1,7 @@
 import React from "react"
 
 const Error = ({message = '', onClick, hide}) => {
-  const baseClass = 'w-4/5 md:max-w-3/5 md:w-auto transition ease-in-out bg-red-300 text-red-500 rounded-lg p-4 fixed left-1/2 -translate-x-1/2 z-[60]';
+  const baseClass = 'break-words w-4/5 md:max-w-3/5 md:w-auto transition ease-in-out bg-red-300 text-red-500 rounded-lg p-4 fixed left-1/2 -translate-x-1/2 z-[60]';
   if (message === '' || hide) {
     return (<div onClick={onClick} className={`${baseClass} -translate-y-48`}>
       <strong>Error:</strong> {message}
